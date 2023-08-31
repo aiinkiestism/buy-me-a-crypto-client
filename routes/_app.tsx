a52db2b3b5e73d51517d2f43e78df2c6c7faee26
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2023-08-31 18:52:05 +0900

    fix: title

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,7 +8,7 @@ export default defineApp(async (req, ctx) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="buy me a crypto is a counterpart of buy me a coffee for crypto." />
-        <title>buy-me-a-crypto-client</title>
+        <title>buy me a crypto</title>
         <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
         <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
         <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
--- routes/docs.tsx
+++ routes/docs.tsx
@@ -2,7 +2,7 @@ import { Header, Footer } from '../components/index.ts';
 
 export default function Docs() {
   return (
-    <div className="bg-[#1E293B] w-full h-screen relative">
+    <div className="w-full h-screen relative bg bg-gradient-to-r from-pink-400 to-purple-400">
       <Header />
       <p class="text-white text-center">WIP, hang in tight!</p>
       <Footer />
